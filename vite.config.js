d080cdb6da3b49e02e8d114d620811db0c6a3595
proindra <prajwalindrakh@gmail.com>
2025-11-13 00:19:43 +0530

    secondcommit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/web/', // 👈 must match your GitHub repo name exactly
 })
